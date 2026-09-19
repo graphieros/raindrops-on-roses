2f2a937d161c1330e60328de56e8801c0822765d
graphieros <alec.lloyd.probert@gmail.com>
2026-09-19 22:39:23 +0200

    chore(website): update meta tags

--- website/src/components/base/BaseFooter.vue
+++ website/src/components/base/BaseFooter.vue
@@ -4,6 +4,12 @@ import { ref, computed } from 'vue'
 
 <template>
   <footer class="border-t border-app-border py-6 mt-6 text-center text-app-text-muted">
-    Made with <span class="text-rose">love</span> by graphieros
+    Made with <span class="text-rose">love</span> by
+    <a
+      href="https://github.com/graphieros"
+      target="_blank"
+      class="hover:text-app-text transition-colors"
+      >graphieros</a
+    >
   </footer>
 </template>
