874aeee28b969a06664f7f745018fa7968161376
graphieros <alec.lloyd.probert@gmail.com>
2026-09-19 23:43:58 +0200

    feat(website): show gh stars

--- website/src/views/HomeView.vue
+++ website/src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import BaseCode from '@/components/base/BaseCode.vue'
 import logo from '../assets/images/raindrops_on_roses.png'
+import { useMainStore } from '@/stores/main'
+
+const store = useMainStore()
 </script>
 
 <template>
@@ -18,8 +21,10 @@ import logo from '../assets/images/raindrops_on_roses.png'
           tested and made with love.
         </p>
 
+        <BaseCode class="w-fit mt-8" :code="`npm i raindrops-on-roses`" language="bash" />
+
         <div
-          class="mt-8 flex flex-col items-center justify-center gap-3 sm:flex-row md:justify-start"
+          class="mt-3 flex flex-col items-center justify-center gap-3 sm:flex-row md:justify-start"
         >
           <button
             class="inline-flex items-center justify-center border border-app-border px-5 py-3 text-sm font-semibold text-app-text transition bg-rose-dark hover:bg-rose cursor-pointer"
@@ -30,13 +35,21 @@ import logo from '../assets/images/raindrops_on_roses.png'
           <button
             class="inline-flex items-center justify-center border border-app-border px-5 py-3 text-sm font-semibold text-app-text transition hover:bg-app-background-slight cursor-pointer"
           >
-            <a href="https://github.com/graphieros/raindrops-on-roses" target="_blank">
+            <a
+              href="https://github.com/graphieros/raindrops-on-roses"
+              target="_blank"
+              class="flex flex-row gap-2 items-center tabular-nums"
+            >
               GitHub repository
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="#be3d5f">
+                <path
+                  d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z"
+                />
+              </svg>
+              {{ store.stars }}
             </a>
           </button>
         </div>
-
-        <BaseCode class="w-fit mt-3" :code="`npm i raindrops-on-roses`" language="bash" />
       </div>
 
       <div class="order-1 flex justify-center md:order-2 md:justify-end">
